d46f5171a63bbd2f949195f1123a0fe6634e3a1c
Wojciech Grochalski <alchemik789@gmail.com>
2021-06-17 15:35:04 +0200

    add bootstrap and starting page v0.2 css change

--- PythonClient/front/rpi-client/src/Components/home/home.component.ts
+++ PythonClient/front/rpi-client/src/Components/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {GPIO} from '../../Models/GPIO';
 import {HttpClient} from '@angular/common/http';
 import {GpioService} from '../../Services/gpio.service';
+import {LoginResult} from '../../Models/LoginResult';
+import {AuthService} from '../../Services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -13,21 +15,26 @@ export class HomeComponent implements OnInit {
   public data: GPIO[];
   leftGpio: GPIO[];
   rightGpio: GPIO[];
+  User: LoginResult;
 
   constructor(
     private http: HttpClient,
-    private gpioService: GpioService) {
+    private gpioService: GpioService,
+    private authService: AuthService ) {
   }
 
-  async ngOnInit(){
-     this.gpioService.GetGpio().subscribe( res => {
-      this.leftGpio = res.splice(0, 20);
-      this.rightGpio = res.splice(0, 20);
-      this.SetColour();
-      console.log(this.rightGpio);
-    });
+  async ngOnInit(): Promise<void>{
+    this.User = this.authService.currentUserValue;
+    if (this.User ) {
+      this.gpioService.GetGpio().subscribe(res => {
+        this.leftGpio = res.splice(0, 20);
+        this.rightGpio = res.splice(0, 20);
+        this.SetColour();
+        console.log(this.rightGpio);
+      });
+    }
   }
-  SetColour(){
+  SetColour(): void{
   this.leftGpio.forEach( (item) => {
     if (item.GPIOName.includes('GPIO')){
       item.color = 'green';
